3a954b74dfaae41d3a5585e560b5ee4607d69c53
DESKTOP-QVOJ483\iibi203 <jseong444@naver.com>
2023-02-15 14:44:23 +0900

    메인메뉴 버튼 오류 수정, 타이틀 UI 인풋 시스템 나오게 추가

--- Assets/Scripts/UI/TitleUI.cs
+++ Assets/Scripts/UI/TitleUI.cs
@@ -3,19 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class TitleUI : MonoBehaviour
 {
     public Button NextBtn;
     ScenesManager sM;
-
+    public TMP_InputField inputField;
+    string playerName;
+    public GameObject inputFieldPanel;
+    public Button StartBtn;
+    public TMP_Text Warningtxt;
     void Start()
     {
         sM = ScenesManager.GetInstance();
-        NextBtn.onClick.AddListener(OnClickStart);
+        inputFieldPanel.SetActive(false);
+        NextBtn.onClick.AddListener(InputFieldPanelOn);
+        StartBtn.onClick.AddListener(OnClickStart);
+    }
+
+    void InputFieldPanelOn()
+    {
+        NextBtn.gameObject.SetActive(false);
+        inputFieldPanel.SetActive(true);
     }
     void OnClickStart()
     {
-        ScenesManager.GetInstance().ChangeScene(Scenes.MenuScene);
+        if (inputField.text != null)
+        {
+            Warningtxt.gameObject.SetActive(false);
+            inputField.text = playerName;
+            PlayerPrefs.SetString("PlayerName", playerName);
+            ScenesManager.GetInstance().ChangeScene(Scenes.MenuScene);
+        }
+        else
+        { 
+            Warningtxt.gameObject.SetActive(true); 
+        }
     }
 }
